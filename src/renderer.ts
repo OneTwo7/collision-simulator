d89ec98d1eb3a138848f96e45a6de4635fcb4187
OneTwo7 <lahoohal@gmail.com>
2024-03-14 15:09:19 +0400

    border-colliding balls

--- src/renderer.ts
+++ src/renderer.ts
@@ -27,5 +27,69 @@
  */
 
 import './index.css';
+import { Ball } from './types';
 
-console.log('👋 This message is being logged by "renderer.ts", included via Vite');
+const CANVAS_WIDTH = 500;
+const CANVAS_HEIGHT = 500;
+const NUMBER_OF_BALLS = 50;
+const BALL_RADIUS = 5;
+const BALL_SPEED = 2.5;
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+const balls: Ball[] = [];
+
+function getSign() {
+  return Math.random() > 0.5 ? 1 : -1;
+}
+
+for (let i = 0; i < NUMBER_OF_BALLS; i++) {
+  const xSpeed = Math.random() * BALL_SPEED;
+  const ySpeed = Math.sqrt(BALL_SPEED ** 2 - xSpeed ** 2);
+
+  balls.push({
+    x: Math.random() * (CANVAS_WIDTH - 10) + 5,
+    y: Math.random() * (CANVAS_HEIGHT - 10) + 5,
+    dir: [xSpeed * getSign(), ySpeed * getSign()],
+  });
+}
+
+function drawBall(x: number, y: number, radius: number) {
+  ctx.beginPath();
+  ctx.arc(x, y, radius, 0, 2 * Math.PI);
+  ctx.closePath();
+  ctx.fill();
+}
+
+function handleBorderCollision(ball: Ball) {
+  const { x, y } = ball;
+
+  if (x + BALL_RADIUS >= CANVAS_WIDTH || x - BALL_RADIUS <= 0) {
+    ball.dir[0] *= -1;
+  }
+
+  if (y + BALL_RADIUS >= CANVAS_HEIGHT || y - BALL_RADIUS <= 0) {
+    ball.dir[1] *= -1;
+  }
+}
+
+function draw() {
+  const date = new Date();
+
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+  for (const ball of balls) {
+    ball.x += ball.dir[0];
+    ball.y += ball.dir[1];
+
+    drawBall(ball.x, ball.y, BALL_RADIUS);
+    handleBorderCollision(ball);
+  }
+
+  window.requestAnimationFrame(draw);
+}
+
+function init() {
+  window.requestAnimationFrame(draw);
+}
+
+init();
---
+++ src/types.ts
@@ -0,0 +1,5 @@
+export type Ball = {
+  x: number;
+  y: number;
+  dir: number[];
+};
